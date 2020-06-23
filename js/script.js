d51cf2f1e88efd7ac268990fe210b0611352277e
Mateusz Golda <mateusz.golda1993@gmail.com>
2020-06-23 15:36:57 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+    // alert("dupa")
+    // console.log("dupa")
+    const x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+// document.getElementById("clickme").addEventListener("click", () => {
+//     myFunction();
+// })
