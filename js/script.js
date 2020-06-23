ad29f15616f689db8a84e45431b85c0e3b698cbe
Mateusz Golda <mateusz.golda1993@gmail.com>
2020-06-23 17:40:26 +0200

    add hamburger menu icon to mentormain.html

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function myFunction() {
-    // alert("dupa")
-    // console.log("dupa")
     const x = document.getElementById("myLinks");
     if (x.style.display === "block") {
         x.style.display = "none";
