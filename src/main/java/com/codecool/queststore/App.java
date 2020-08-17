292c64d83f1ad435ac2dc97551d2d29fe704b8f2
Grelory <gmpwm99@gmail.com>
2020-08-17 14:50:08 +0200

    add initial server setting

--- src/main/java/com/codecool/queststore/App.java
+++ src/main/java/com/codecool/queststore/App.java
@@ -1,7 +1,27 @@
 package com.codecool.queststore;
 
-public class App {
-    public static void main(String[] args) {
+import com.codecool.queststore.user.admin.AdminController;
+import com.codecool.queststore.user.login.LoginController;
+import com.codecool.queststore.user.mentor.MentorController;
+import com.codecool.queststore.user.student.StudentController;
+import com.sun.net.httpserver.HttpServer;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class App {
+
+    public static void main(String[] args) throws IOException {
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        // set routes
+        server.createContext("/student", new StudentController());
+        server.createContext("/mentor", new MentorController());
+        server.createContext("/admin", new AdminController());
+        server.createContext("/login", new LoginController());
+
+
+        // start
+        server.start();
     }
 }
