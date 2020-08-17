d155a27929acf054e074100d87ecdb3c04e29e6c
MateuszGolda <mateusz.golda1993@gmail.com>
2020-08-17 12:07:59 +0300

    reorganize project structure

---
+++ src/main/java/com/codecool/queststore/App.java
@@ -0,0 +1,7 @@
+package com.codecool.queststore;
+
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/com/codecool/queststore/PostgreSqlJDBC.java
@@ -0,0 +1,22 @@
+package com.codecool.queststore;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class PostgreSqlJDBC {
+    private Connection c = null;
+
+    public Connection getConnection(String url, String user, String password) {
+        if (c == null) {
+            try {
+                c = DriverManager.getConnection(url, user, password); // set user and password
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.err.println(e.getClass().getName() + ": " + e.getMessage());
+                System.exit(0);
+            }
+            System.out.println("Opened database successfully");
+        }
+        return c;
+    }
+}
