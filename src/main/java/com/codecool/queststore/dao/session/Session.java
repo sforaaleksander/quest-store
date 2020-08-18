4a482bf794588b9a0fd8b78b4a96fc9a8a325275
Grelory <gmpwm99@gmail.com>
2020-08-18 22:51:06 +0200

    add sessionDao

---
+++ src/main/java/com/codecool/queststore/dao/session/Session.java
@@ -0,0 +1,40 @@
+package com.codecool.queststore.dao.session;
+
+import java.sql.Timestamp;
+
+public class Session {
+
+    private String sessionId;
+    private int userId;
+    private Timestamp loginTimestamp;
+    private Timestamp logoutTimestamp;
+    private boolean active;
+
+    public Session(String sessionId, int userId, Timestamp loginTimestamp, Timestamp logoutTimestamp, boolean active) {
+        this.sessionId = sessionId;
+        this.userId = userId;
+        this.loginTimestamp = loginTimestamp;
+        this.logoutTimestamp = logoutTimestamp;
+        this.active = active;
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public Timestamp getLoginTimestamp() {
+        return loginTimestamp;
+    }
+
+    public Timestamp getLogoutTimestamp() {
+        return logoutTimestamp;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+}
