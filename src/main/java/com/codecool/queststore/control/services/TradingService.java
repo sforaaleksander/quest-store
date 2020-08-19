b28c08dd0fe7eb67ca5eb85d4e0766d6dfee7d5a
pietia <piotrstepak@gmail.com>
2020-08-19 19:22:39 +0200

    Add getStudentCoinsAmount method

--- src/main/java/com/codecool/queststore/control/services/TradingService.java
+++ src/main/java/com/codecool/queststore/control/services/TradingService.java
@@ -1,8 +1,12 @@
 package com.codecool.queststore.control.services;
 
 import com.codecool.queststore.dao.Dao;
+import com.codecool.queststore.dao.balance.Balance;
+import com.codecool.queststore.dao.balance.BalanceDao;
 import com.codecool.queststore.dao.items.Item;
 import com.codecool.queststore.dao.items.ItemDao;
+import com.codecool.queststore.dao.quests.Quest;
+import com.codecool.queststore.dao.quests.QuestDao;
 import com.codecool.queststore.dao.userItems.UserItems;
 import com.codecool.queststore.dao.userItems.UserItemsDao;
 import com.codecool.queststore.dao.userQuests.UserQuests;
@@ -16,9 +20,11 @@ public class TradingService {
     private final UserItemsDao userItemDao = new UserItemsDao();
     private final ItemDao itemDao = new ItemDao();
     private Dao<UserQuests> userQuestDao = new UserQuestsDao();
+    private Dao<Balance> balanceDao = new BalanceDao();
+    private Dao<Quest> questDao = new QuestDao();
 
-    public int getStudentCoinsAmount() {
-        return 0;
+    public int getStudentCoinsAmount(int userId) {
+        return balanceDao.get(String.format("user_id=?", userId)).get(0).getAmount();
     }
 
     public boolean addUserQuest(int questId, int userId) {
@@ -30,14 +36,13 @@ public class TradingService {
         UserQuests userQuestToAccept = userQuestDao.get(String.format("id=%d LIMIT 1", userQuestId)).get(0);
         userQuestToAccept.setAccepted(true);
         return userQuestDao.update(userQuestToAccept);
-
     }
 
     public boolean addUserItem(int itemId, int userId) {
         Item itemToBuy = itemDao.get(String.format("id=%d LIMIT 1", itemId)).get(0);
-        if (itemToBuy.getCategoryId() == 0) return makeGroupShopping(); // TODO check if item is bought by many students
+        if (itemToBuy.getCategoryId() == 4) return makeGroupShopping();
         int itemCost = itemToBuy.getCost();
-        int userBalance = 50; // TODO get student's balance
+        int userBalance = getStudentCoinsAmount(userId);
         if (userBalance < itemCost) return false;
         return userItemDao.insert(new UserItems().setItemId(itemToBuy.getId()).setUserId(userId)
                 .setBoughtDate(Date.valueOf(LocalDate.now())).setUsed(false));
