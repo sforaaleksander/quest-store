f1f9be5ed88dc9396151d8b3968242fcc0f5bf12
Grelory <gmpwm99@gmail.com>
2020-08-19 17:20:51 +0200

    implement useUserItem() acceptUserQuest() and addUserQuest(), add getStudentCoinsAmount()

--- src/main/java/com/codecool/queststore/control/services/TradingService.java
+++ src/main/java/com/codecool/queststore/control/services/TradingService.java
@@ -1,21 +1,43 @@
 package com.codecool.queststore.control.services;
 
+import com.codecool.queststore.dao.Dao;
+import com.codecool.queststore.dao.userItems.UserItems;
+import com.codecool.queststore.dao.userItems.UserItemsDao;
+import com.codecool.queststore.dao.userQuests.UserQuests;
+import com.codecool.queststore.dao.userQuests.UserQuestsDao;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
 public class TradingService {
 
-    public void addUserQuest() {
+    private final UserItemsDao userItemDao = new UserItemsDao();
+    private Dao<UserQuests> userQuestDao = new UserQuestsDao();
+
+    public int getStudentCoinsAmount() {
+        return 0;
+    }
+
+    public boolean addUserQuest(int questId, int userId) {
+        return userQuestDao.insert(new UserQuests().setQuestId(questId).setUserId(userId)
+                .setDoneDate(Date.valueOf(LocalDate.now())).setAccepted(false));
+    }
+
+    public boolean acceptUserQuest(int userQuestId) {
+        UserQuests userQuestToAccept = userQuestDao.get(String.format("id=%d LIMIT 1", userQuestId)).get(0);
+        userQuestToAccept.setAccepted(true);
+        return userQuestDao.update(userQuestToAccept);
 
     }
 
-    public void acceptUserQuest() {
+    public void addUserItem(int itemId, int userId) {
 
     }
 
-    public void addUserItem() {
-
-    }
-
-    public void useUserItem() {
-
+    public boolean useUserItem(int userItemId) {
+        UserItems userItemToUse = userItemDao.get(String.format("id=%d LIMIT 1", userItemId)).get(0);
+        userItemToUse.setUsed(true);
+        return userItemDao.update(userItemToUse);
     }
 
     public void addStudentsShopping() {
