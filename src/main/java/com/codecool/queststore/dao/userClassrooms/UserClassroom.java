afb159908137e7e5a682cab745200827a90da14b
Grelory <gmpwm99@gmail.com>
2020-08-19 15:07:22 +0200

    change contructor into builder

--- src/main/java/com/codecool/queststore/dao/userClassrooms/UserClassroom.java
+++ src/main/java/com/codecool/queststore/dao/userClassrooms/UserClassroom.java
@@ -1,19 +1,25 @@
 package com.codecool.queststore.dao.userClassrooms;
 
 public class UserClassroom {
+
     private int userId;
     private int classroomId;
-
-    public UserClassroom(int userId, int classroomId) {
-        this.userId = userId;
-        this.classroomId = classroomId;
-    }
 
     public int getUserId() {
         return userId;
     }
 
+    public UserClassroom setUserId(int userId) {
+        this.userId = userId;
+        return this;
+    }
+
     public int getClassroomId() {
         return classroomId;
+    }
+
+    public UserClassroom setClassroomId(int classroomId) {
+        this.classroomId = classroomId;
+        return this;
     }
 }
