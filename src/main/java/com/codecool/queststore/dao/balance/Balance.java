8bca0965978a6d6589278c3f3441bf2d99312afb
pietia <piotrstepak@gmail.com>
2020-08-19 18:06:21 +0200

    Add Balance and BalanceDao classes

---
+++ src/main/java/com/codecool/queststore/dao/balance/Balance.java
@@ -0,0 +1,37 @@
+package com.codecool.queststore.dao.balance;
+
+public class Balance {
+    private int userId;
+    private int amount;
+    private int totalEarned;
+
+    public Balance(){
+    }
+
+    public Balance setUserId(int userId) {
+        this.userId = userId;
+        return this;
+    }
+
+    public Balance setAmount(int amount) {
+        this.amount = amount;
+        return this;
+    }
+
+    public Balance setTotalEarned(int totalEarned) {
+        this.totalEarned = totalEarned;
+        return this;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public int getTotalEarned() {
+        return totalEarned;
+    }
+}
---
+++ src/main/java/com/codecool/queststore/dao/balance/BalanceDao.java
@@ -0,0 +1,100 @@
+package com.codecool.queststore.dao.balance;
+
+import com.codecool.queststore.dao.Dao;
+import com.codecool.queststore.dao.PostgreSqlJDBC;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BalanceDao extends PostgreSqlJDBC implements Dao<Balance> {
+
+    @Override
+    public List<Balance> get(String condition) {
+        List<Balance> balances = new ArrayList<>();
+        try {
+            Statement statement = getConnection().createStatement();
+            ResultSet resultSet = statement.executeQuery(
+                    String.format("SELECT * FROM balance WHERE %s;", condition));
+            balances = createBalancesFromResultSet(resultSet);
+            statement.close();
+            resultSet.close();
+            closeConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return balances;
+    }
+
+    private List<Balance> createBalancesFromResultSet(ResultSet resultSet) throws SQLException {
+        List<Balance> balanceList = new ArrayList<>();
+        Balance balance;
+        while (resultSet.next()) {
+            int userId = resultSet.getInt("user_id");
+            int amount = resultSet.getInt("amount");
+            int totalEarned = resultSet.getInt("total_earned");
+            balance = new Balance().setUserId(userId).setAmount(amount).setTotalEarned(totalEarned);
+            balanceList.add(balance);
+        }
+        return balanceList;
+    }
+
+    @Override
+    public boolean insert(Balance balance) {
+        try {
+            String insertTemplate = "INSERT INTO balance (user_id, amount, total_earned) " +
+                    "VALUES (?, ?, ?);";
+            PreparedStatement preparedStatement = getConnection().prepareStatement(insertTemplate);
+            preparedStatement.setInt(1, balance.getUserId());
+            preparedStatement.setInt(2, balance.getAmount());
+            preparedStatement.setInt(3, balance.getTotalEarned());
+            preparedStatement.execute();
+            preparedStatement.close();
+            closeConnection();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    public boolean update(Balance balance) {
+        try {
+            String updateTemplate = "UPDATE balance " +
+                    "SET amount=?, total_earned=? " +
+                    "WHERE user_id=?;";
+            PreparedStatement preparedStatement = getConnection().prepareStatement(updateTemplate);
+            preparedStatement.setInt(1, balance.getAmount());
+            preparedStatement.setInt(2, balance.getTotalEarned());
+            preparedStatement.setInt(3, balance.getUserId());
+
+            preparedStatement.executeUpdate();
+            preparedStatement.close();
+            closeConnection();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    @Override
+    public boolean delete(Balance balance) {
+        try {
+            String deleteTemplate = "DELETE FROM balance WHERE user_id=?;";
+            PreparedStatement preparedStatement = getConnection().prepareStatement(deleteTemplate);
+            preparedStatement.setInt(1, balance.getUserId());
+            preparedStatement.execute();
+            preparedStatement.close();
+            closeConnection();
+            return true;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
