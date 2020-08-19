f91b8f43122a431ef2523634a07caec1981b3d5f
Grelory <gmpwm99@gmail.com>
2020-08-19 10:40:38 +0200

    add services structure

---
+++ src/main/java/com/codecool/queststore/control/services/ShopService.java
@@ -0,0 +1,34 @@
+package com.codecool.queststore.control.services;
+
+public class ShopService {
+
+    public void addItem() {
+
+    }
+
+    public void editItem() {
+
+    }
+
+    public void addQuest() {
+
+    }
+
+    public void editQuest() {
+
+    }
+
+    public void addCategory() {
+
+    }
+
+    public void addObjectToCategory() {
+
+    }
+
+    public void changeObjectCategory() {
+
+    }
+
+
+}
---
+++ src/main/java/com/codecool/queststore/control/services/UserService.java
@@ -0,0 +1,33 @@
+package com.codecool.queststore.control.services;
+
+import com.codecool.queststore.dao.Dao;
+import com.codecool.queststore.user.User;
+import com.codecool.queststore.user.UserDao;
+
+public class UserService {
+
+    public void addStudent(String... data) {
+
+    }
+
+    public void addMentor(String... someData) {
+
+    }
+
+    public void editMentorProfile(String data) {
+
+    }
+
+    public Object seeMentorProfile(String data) {
+        return null;
+    }
+
+    public void createClassroom(String data) {
+
+    }
+
+    public void addUserToClassroom(String... data) {
+
+    }
+
+}
