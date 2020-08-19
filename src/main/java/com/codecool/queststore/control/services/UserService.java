eb26fafec5a0961ce7abd7c245177a622e64b806
Grelory <gmpwm99@gmail.com>
2020-08-19 14:42:55 +0200

    implement UserServices all methods

--- src/main/java/com/codecool/queststore/control/services/UserService.java
+++ src/main/java/com/codecool/queststore/control/services/UserService.java
@@ -1,48 +1,63 @@
 package com.codecool.queststore.control.services;
 
 import com.codecool.queststore.dao.Dao;
-import com.codecool.queststore.dao.PostgreSqlJDBC;
+import com.codecool.queststore.dao.classrooms.Classroom;
+import com.codecool.queststore.dao.classrooms.ClassroomDao;
 import com.codecool.queststore.dao.roles.Role;
 import com.codecool.queststore.dao.roles.RoleDao;
 import com.codecool.queststore.dao.user.User;
 import com.codecool.queststore.dao.user.UserDao;
+import com.codecool.queststore.dao.userClassrooms.UserClassroom;
+import com.codecool.queststore.dao.userClassrooms.UserClassroomDao;
+
+import java.util.List;
 
 public class UserService {
 
     private Dao<User> userDao = new UserDao();
     private Dao<Role> roleDao = new RoleDao();
+    private Dao<Classroom> classroomDao = new ClassroomDao();
+    private Dao<UserClassroom> userClassroomDao = new UserClassroomDao();
+    private final int MENTOR_ROLE_ID = roleDao.get("name='mentor' LIMIT 1").get(0).getId();
+    private final int STUDENT_ROLE_ID = roleDao.get("name='student' LIMIT 1").get(0).getId();
 
-    public void addStudent(String name, String surname, String password, String email) {
-        addUser(name, surname, password, email, roleDao.get("name='student' LIMIT 1").get(0).getId());
+    public boolean addStudent(String name, String surname, String password, String email) {
+        return addUser(name, surname, password, email, STUDENT_ROLE_ID);
     }
 
-    public void addMentor(String name, String surname, String password, String email) {
-        addUser(name, surname, password, email, roleDao.get("name='mentor' LIMIT 1").get(0).getId());
+    public boolean addMentor(String name, String surname, String password, String email) {
+        return addUser(name, surname, password, email, MENTOR_ROLE_ID);
     }
 
-    private void addUser(String name, String surname, String password, String email, int roleId) {
-        userDao.insert(new User().setName(name).setSurname(surname).setPassword(password)
+    private boolean addUser(String name, String surname, String password, String email, int roleId) {
+        return userDao.insert(new User().setName(name).setSurname(surname).setPassword(password)
                 .setEmail(email).setIdRole(roleId).setActive(true));
     }
 
-    public void editMentorProfile(int mentorId, String name, String surname, String password,
+    public boolean editMentorProfile(int mentorId, String name, String surname, String password,
                                   String email, boolean isActive) {
         User mentorToEdit = userDao.get(String.format("id=%d LIMIT 1", mentorId)).get(0);
         mentorToEdit.setName(name).setSurname(surname).setPassword(password)
                 .setEmail(email).setActive(isActive);
-        userDao.update(mentorToEdit);
+        return userDao.update(mentorToEdit);
     }
 
-    public Object seeMentorProfile(String data) {
-        return null;
+    public User getMentor(int mentorId) {
+        return userDao.get(String.format("id=%d AND id_role=%d LIMIT 1)", mentorId, MENTOR_ROLE_ID)).get(0);
     }
 
-    public void createClassroom(String data) {
-
+    public List<User> getAllMentors() {
+        return userDao.get(String.format("id_role=%d)", MENTOR_ROLE_ID));
     }
 
-    public void addUserToClassroom(String... data) {
+    public boolean createClassroom(String classroomName) {
+        return classroomDao.insert(new Classroom().setName(classroomName));
+    }
 
+    public boolean addUserToClassroom(int userId, String classroomName) {
+        int classroomId = classroomDao.get(
+                String.format("name='%s' LIMIT 1", classroomName)).get(0).getId();
+        return userClassroomDao.insert(new UserClassroom(userId, classroomId));
     }
 
 }
