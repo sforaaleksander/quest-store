d69e94f7fe244bda7086e504df505589a7a5078f
MateuszGolda <mateusz.golda1993@gmail.com>
2020-08-20 19:17:35 +0300

    add sslmode=require, to fix db connection, resign from singleton pattern

--- src/main/java/com/codecool/queststore/dao/PostgreSqlJDBC.java
+++ src/main/java/com/codecool/queststore/dao/PostgreSqlJDBC.java
@@ -8,20 +8,19 @@ public class PostgreSqlJDBC {
     private static Connection connection = null;
 
     public Connection getConnection() {
-        final String url = "jdbc:postgresql://ec2-54-75-244-161.eu-west-1.compute.amazonaws.com:5432/da9vidtssmmkj9";
+        final String url = "jdbc:postgresql://ec2-54-75-244-161.eu-west-1.compute.amazonaws.com:5432/da9vidtssmmkj9?sslmode=require";
         final String user = "psvvypnkwffifs";
         final String password = "5a21cf41def123115cdeaaa367e70f31907b2b0b73a1f643b212e2e8bf097327";
 
-        if (connection == null) {
-            try {
-                connection = DriverManager.getConnection(url, user, password); // set user and password
-            } catch (SQLException e) {
-                e.printStackTrace();
-                System.err.println(e.getClass().getName() + ": " + e.getMessage());
-                throw new RuntimeException("Couldn't open database");
-            }
-            System.out.println("Opened database successfully");
+        try {
+            connection = DriverManager.getConnection(url, user, password); // set user and password
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+            throw new RuntimeException("Couldn't open database");
         }
+        System.out.println("Opened database successfully");
+
         return connection;
     }
 
