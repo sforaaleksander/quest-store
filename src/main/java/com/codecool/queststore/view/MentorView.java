8ec4d2dfcfe6a3cbe731ab05eedb8e2b2d7fc523
pietia <piotrstepak@gmail.com>
2020-08-20 17:01:58 +0200

    Standardize methods

--- src/main/java/com/codecool/queststore/view/MentorView.java
+++ src/main/java/com/codecool/queststore/view/MentorView.java
@@ -6,26 +6,36 @@ import com.sun.net.httpserver.HttpExchange;
 import org.jtwig.JtwigModel;
 import org.jtwig.JtwigTemplate;
 
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.List;
 
 public class MentorView {
 
-    public void loadMainPageTemplateWithMentor(HttpExchange exchange, User mentor) {
+    public void loadMainPageTemplateWithMentor(HttpExchange exchange, User mentor) throws IOException {
         JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/mentor/mentorMain.twig");
         JtwigModel model = JtwigModel.newModel().with("mentor", mentor);
-        String response = template.render(model);
+        renderSendGetWriteClose(template, model, exchange);
     }
 
-    public void loadTemplateWithAllStudents(HttpExchange exchange, List<User> students) {
+    public void loadTemplateWithAllStudents(HttpExchange exchange, List<User> students) throws IOException {
         JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/mentor/studentLists.twig");
         JtwigModel model = JtwigModel.newModel().with("students", students);
-        String response = template.render(model);
+        renderSendGetWriteClose(template, model, exchange);
     }
 
-    public void loadTemplateWithAssignStudentToClassroom(HttpExchange exchange, User student, List<Classroom> classrooms) {
+    public void loadTemplateWithAssignStudentToClassroom(HttpExchange exchange, User student, List<Classroom> classrooms) throws IOException {
         JtwigTemplate template = JtwigTemplate.classpathTemplate("templates/mentor/studentLists.twig");
         JtwigModel model = JtwigModel.newModel().with("students", student)
                                                 .with("classrooms",classrooms);
+        renderSendGetWriteClose(template, model, exchange);
+    }
+
+    private void renderSendGetWriteClose(JtwigTemplate template, JtwigModel model, HttpExchange exchange) throws IOException {
         String response = template.render(model);
+        exchange.sendResponseHeaders(200, response.length());
+        OutputStream os = exchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
     }
 }
