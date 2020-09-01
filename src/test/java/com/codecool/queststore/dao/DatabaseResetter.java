197c34d990cf9eaf114845c2231ecc5df35ce867
Rafal Wypasek <rafal.zawodowiec@gmail.com>
2020-09-01 18:51:02 +0200

    access modifies set to public

--- src/test/java/com/codecool/queststore/dao/DatabaseResetter.java
+++ src/test/java/com/codecool/queststore/dao/DatabaseResetter.java
@@ -9,19 +9,20 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-class DatabaseResetter {
+public class DatabaseResetter {
+
 
     private static final String URL = "jdbc:h2:~/test_db";
     private static final String USER = "test";
     private static final String PASSWORD = "";
 
-    private static Connection getConnection() throws SQLException {
+    public Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 
-    public static void resetDatabase() throws SQLException {
+    public void resetDatabase() throws SQLException {
         clearDB();
-        Connection connection = DatabaseResetter.getConnection();
+        Connection connection = this.getConnection();
         try {
             RunScript.execute(connection, new FileReader("src/main/resources/sql/jdbc_test_db.sql"));
             connection.close();
@@ -31,8 +32,8 @@ class DatabaseResetter {
         }
     }
 
-    public static void clearDB() throws SQLException {
-        Connection connection = DatabaseResetter.getConnection();
+    public void clearDB() throws SQLException {
+        Connection connection = this.getConnection();
         Statement statement = connection.createStatement();
         String query = " DROP ALL OBJECTS;";
         statement.executeUpdate(query);
